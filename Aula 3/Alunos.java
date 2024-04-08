9e22b63b148b870b375ae56442dfebca0fd890eb
MarcoAntonioUFG <146884614+MarcoAntonioUFG@users.noreply.github.com>
2024-04-08 15:30:13 -0300

    Add files via upload

---
+++ Aula 3/Alunos.java
@@ -0,0 +1,45 @@
+public class Alunos {
+    private String nome;
+    private int matricula;
+    private int ano;
+    Curso curso;
+
+    public Alunos(String nome, int matricula, int ano, Curso curso) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.ano = ano;
+        this.curso = curso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+}
